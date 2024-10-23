043570a512c96799717ac02385f36d02086daab9
Musamasaba98 <masamata98@gmail.com>
2024-10-23 22:29:15 +0300

    Basic setup of the test suite entry point

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-alert("Hello World");
+import kcfDefaultStyles from "./stylesheets/style.css";
+import kcfTestUIstyles from "./stylesheets/kcf-test-ui.css";
+import kcfTestTogglerStyles from "./stylesheets/kcf-test-toggler.css";
+import kcfMochaModalStyles from "./stylesheets/mocha-modal.css";
+
+const shadow = (() => {
+  const shadowHost = document.createElement("div");
+  shadowHost.setAttribute("id", "kcf_test_suite_wrapper");
+  shadowHost.style.setProperty("position", "relative", "important");
+  shadowHost.style.setProperty("z-index", "9999", "important");
+  document.body.appendChild(shadowHost);
+  const supportsShadowDOMV = !!HTMLElement.prototype.attachShadow;
+  let shadow;
+  if (supportsShadowDOMV) {
+    shadow = shadowHost.attachShadow({ mode: "open" });
+  } else {
+    shadow = shadowHost;
+  }
+  return shadow;
+})();
